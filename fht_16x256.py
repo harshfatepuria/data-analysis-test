9cca15fee978004ef3cf5f6164a402161685ec40
Rahul Agrawal <rahulagr2000@gmail.com>
2016-02-24 10:33:56 -0800

    Updated FHT Analysis
    Note: It has to be done on all data. This program will take only 75 %
    training data. Also, Check output file format

--- fht_16x256.py
+++ fht_16x256.py
@@ -10,34 +10,40 @@ typeJson = json.loads(input_file.read())
 input_file.close()
 
 # print typeJson['train'][0]
-byteFreqSignature=[]
-for i in range(0,256):
-       byteFreqSignature.append(0)
-
 sparseMatrix=[]
-colm=[]
-for i in range(0,256):
-       colm.append(0)
-
 for i in range(0,16):
-    sparseMatrix.append(colm)
+    sparseMatrix.append([])
+    for j in range(0,256):
+       sparseMatrix[i].append(0)
     
-output_file=open('application%2Ffht_16x256.txt','a') 
+output_file=open('application%2Ffht_16x256.json','w') 
 countFile=0
+
 for k in range(0,len(typeJson['test'])):
     filePath=""+typeJson['test'][k]
-
-    input_file=open(filePath,'rb')
-    data=input_file.read()
-    if(len(data)<16): continue # In case a file has no header bytes! Ignoring here.
+    try:
+        input_file=open(filePath,'rb')
+        data=input_file.read()
+    except:
+        continue
+    if(len(data)<16): 
+        continue # In case a file has no header bytes! Ignoring here.
+    
     for j in range(0,16):
         x=ord(data[j:(j+1)])
         sparseMatrix[j][x]=sparseMatrix[j][x]+1
-        countFile=countFile+1
+    
+    countFile=countFile+1
+    
 if countFile!=0: 
     for i in range(16):
         for j in range(256):
-            sparseMatrix[i][j]=sparseMatrix[i][j]/countFile
-        
-output_file.write(sparseMatrix)
+            sparseMatrix[i][j]=float(sparseMatrix[i][j])/float(countFile)
+
+sparseMatrixDict={}
+for i in range(16):
+    sparseMatrixDict[i]=sparseMatrix[i]
+keys=json.dumps(sparseMatrixDict, sort_keys=True)
+output_file.write(keys)
+#output_file.write(sparseMatrixDict)
 output_file.close()

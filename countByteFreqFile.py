1be7228ef0244ad458694eabd11fc2091c33beaf
Harsh Fatepuria <fatepuri@usc.edu>
2016-02-24 22:14:43 -0800

    Final BFA Signature generator for all 15 types

--- countByteFreqFile.py
+++ countByteFreqFile.py
@@ -1,42 +1,55 @@
 import json
-
-input_file=open('chosenFileTypes//application%2Frtf.json','r')
-typeJson = json.loads(input_file.read())
-input_file.close()
-
-# print typeJson['train'][0]
-byteFreqSignature=[]
-for i in range(0,256):
-   	byteFreqSignature.append(0)
-
-byteFreq=[]
-for i in range(0,256):
-   	byteFreq.append(0)
-
-for k in range(0,len(typeJson['train'])):
-    #filePath='anagram_out.txt'
-    filePath=""+typeJson['train'][k]
-    for i in range(0,256):
-        byteFreq[i]=0
-
-	input_file=open(filePath,'rb')
-	data=input_file.read()
-    for j in range(0,len(data)):
-        x=ord(data[j:(j+1)])
-    	byteFreq[x]=byteFreq[x]+1
-
-    maxFreq=max(byteFreq)
-    
-    for i in range(0,256):
-    	byteFreq[i]=byteFreq[i]/maxFreq
-    	byteFreqSignature[i]=byteFreqSignature[i]+byteFreq[i]
-
-maxFreq=max(byteFreqSignature)
-for i in range(0,256):
-    byteFreqSignature[i]=byteFreqSignature[i]/maxFreq
-    print i," ",byteFreqSignature[i]
-
-keys=json.dumps(byteFreqSignature, sort_keys=True)
-output_file=open('application%2Frtf_signature.json','w')
-output_file.write(keys)
-output_file.close()
+from glob import iglob
+import os
+from compiler.pycodegen import EXCEPT
+fileR='chosenFileTypes'
+  
+sourcePath="/Volumes/HARSH/CS599/ORIGINAL/"  
+  
+for filepath in iglob(os.path.join(fileR, '*.json')): 
+    with open(filepath) as f:
+        typeJson = json.loads(f.read())
+  
+        byteFreqSignature=[]
+        for i in range(0,256):
+            byteFreqSignature.append(0)
+   
+        byteFreq=[]
+        for i in range(0,256):
+            byteFreq.append(0)
+   
+        for k in range(0,len(typeJson['train'])):
+            
+            filePath=sourcePath+typeJson['train'][k]
+            
+            print typeJson['type']," ",k, " ", (typeJson['trainCount']-1),
+            
+            for i in range(0,256):
+                byteFreq[i]=0
+            try:
+                input_file=open(filePath,'rb')
+                data=input_file.read()
+            except:
+                continue
+            print len(data)
+            if len(data)>0:
+                for j in range(0,len(data)):
+                    x=ord(data[j:(j+1)])
+                    byteFreq[x]=byteFreq[x]+1
+   
+                maxFreq=max(byteFreq)
+                for i in range(0,256):
+                    byteFreq[i]=byteFreq[i]/float(maxFreq)
+                    byteFreqSignature[i]=byteFreqSignature[i]+byteFreq[i]
+   
+        maxFreq=max(byteFreqSignature)
+        for i in range(0,256):
+            byteFreqSignature[i]=byteFreqSignature[i]/float(maxFreq)
+   
+        keys=json.dumps(byteFreqSignature, sort_keys=True)
+        typeOfFile=str(typeJson['type'])
+        qq=typeOfFile.find("/")
+        outFile='json_outputs/bfa_signature_'+typeOfFile[0:qq]+"_"+typeOfFile[qq+1:]+'.json'
+        output_file=open(outFile,'w')
+        output_file.write(keys)
+        output_file.close()

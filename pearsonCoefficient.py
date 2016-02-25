e6ed5825d86df2d5fc061c9b3d6e30a451a3e102
Rahul Agrawal <rahulagr2000@gmail.com>
2016-02-25 00:44:32 -0800

    File to calculate Pearson Coefficient on 25 % test data
    calculate Pearson Coefficient on 25 % test data (Each file of test data
    v/s BFA Signature.

---
+++ pearsonCoefficient.py
@@ -0,0 +1,125 @@
+# Generates a file with test file paths and bfc coefficient.
+# BFC Coefficient= -1(Highly dissimilar to +1(very similar)
+
+
+import json
+from glob import iglob
+import os
+from compiler.pycodegen import EXCEPT
+from itertools import imap
+import math
+
+fileR='chosenFileTypes'
+  
+sourcePath="/Volumes/HARSH/CS599/ORIGINAL/"
+
+def bfcPearsonCoeff(x, y):
+    n = len(x)
+    sumX = float(sum(x))
+    sumY = float(sum(y))
+    sumX2 = sum(map(lambda x: pow(x, 2), x))
+    sumY2 = sum(map(lambda x: pow(x, 2), y))
+    psum = sum(imap(lambda x, y: x * y, x, y))
+    a = psum - (sumX * sumY/n)
+    b = pow((sumX2 - pow(sumX, 2) / n) * (sumY2 - pow(sumY, 2) / n), 0.5)
+    if b == 0: return 0
+    return a / b
+  
+  
+for filepath in iglob(os.path.join(fileR, '*.json')): 
+    with open(filepath) as f:
+        typeJson = json.loads(f.read())
+  
+        typeOfFile=str(typeJson['type'])
+        qq=typeOfFile.find("/")
+        
+        f=open('json_outputs/bfa_signature_'+typeOfFile[0:qq]+"_"+typeOfFile[qq+1:]+'.json','r')
+        BFA = json.loads(f.read())
+        f.close()
+   
+        byteFreq=[]
+        for i in range(0,256):
+            byteFreq.append(0)
+   
+        outFile='pearson_Coefficient/bfa_signature_'+typeOfFile[0:qq]+"_"+typeOfFile[qq+1:]+'.json'
+        output_file=open(outFile,'a')
+        
+        
+        for k in range(0,len(typeJson['test'])):
+            
+            filePath=sourcePath+typeJson['test'][k]
+            
+            print typeJson['type']," ",k, " ", (typeJson['trainCount']-1),
+            
+            for i in range(0,256):
+                byteFreq[i]=0
+            try:
+                input_file=open(filePath,'rb')
+                data=input_file.read()
+            except:
+                continue
+            print len(data)
+            if len(data)>0:
+                for j in range(0,len(data)):
+                    x=ord(data[j:(j+1)])
+                    byteFreq[x]=byteFreq[x]+1
+   
+                maxFreq=max(byteFreq)
+                for i in range(0,256):
+                    byteFreq[i]=byteFreq[i]/float(maxFreq)
+                
+                print bfcPearsonCoeff(byteFreq,BFA)
+                stroutput=""
+                stroutput=stroutput+filepath+" "+ bfcPearsonCoeff(byteFreq,BFA)+"\n"
+                output_file.write(stroutput)
+
+                #byteFreqSignature= #Get signature from stored info
+   
+        #maxFreq=max(byteFreqSignature)
+        #for i in range(0,256):
+            #byteFreqSignature[i]=byteFreqSignature[i]/float(maxFreq)
+   
+        #keys=json.dumps(byteFreqSignature, sort_keys=True)
+        #output_file.write(keys)
+        output_file.close()
+
+##############
+
+
+
+
+# print typeJson['train'][0]
+byteFreqSignature=[]
+for i in range(0,256):
+       byteFreqSignature.append(0)
+
+byteFreq=[]
+for i in range(0,256):
+       byteFreq.append(0)
+
+output_file=open('application%2Fbfc.txt','a') # change to name of file to whatever you want
+
+for k in range(0,len(typeJson['test'])):
+    #filePath='anagram_out.txt'
+    filePath=""+typeJson['test'][k]
+    for i in range(0,256):
+        byteFreq[i]=0
+
+    input_file=open(filePath,'rb')
+    data=input_file.read()
+    for j in range(0,len(data)):
+        x=ord(data[j:(j+1)])
+        byteFreq[x]=byteFreq[x]+1
+
+    maxFreq=max(byteFreq)
+    
+    for i in range(0,256):
+        byteFreq[i]=byteFreq[i]/maxFreq
+        #byteFreqSignature[i]=byteFreqSignature[i]+byteFreq[i]
+    #Get BFA of the file type in variable BFA
+    print bfcPearsonCoeff(byteFreq,BFA)
+    stroutput=""
+    stroutput=stroutput+filepath+" "+ bfcPearsonCoeff(byteFreq,BFA)+"\n"
+    output_file.write(stroutput)
+    
+output_file.close()
